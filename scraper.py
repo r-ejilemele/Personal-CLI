11610da5f16dab67a7fdcf00c7c83d5f574a2ff1
r-ejilemele <r.ejilemele@gmail.com>
2024-07-11 06:07:04 -0400

    made Boston herald scraper

--- scraper.py
+++ scraper.py
@@ -7,7 +7,6 @@ from rich.theme import Theme
 from rich.console import Console
 
 
-
 load_dotenv()
 custom_theme = Theme(
     {
@@ -59,8 +58,34 @@ def scrape_cnn():
         for link in links:
             text = link.find("span", class_="container__headline-text")
             if text is not None:
-                articles[text.text] = "https://www.cnn.com" + link.get("href")
+                articles[text.text.strip()] = "https://www.cnn.com" + link.get("href")
                 break
+    return articles
+
+
+def scrape_boston_herald():
+    herald = requests.get("https://www.bostonherald.com/news/")
+    html = BeautifulSoup(herald.text, "lxml")
+    unorderedList = html.find("ul", class_="menu", id="subnav-news-menu")
+    links = unorderedList.findAll("a")
+    pages = [link.get("href") for link in links]
+    pages = [
+        page
+        for page in pages
+        if "opinion" not in page and "traffic" not in page and "business" not in page
+    ]
+    divs = []
+    articles = dict()
+    for page in pages:
+        open_html = BeautifulSoup(requests.get(page).text, "lxml")
+        div = open_html.find("div", class_="tag-content load-more-wrapper")
+        article = div.find("article")
+        article_link = article.find("h2", class_="entry-title").find(
+            "a", class_="article-title"
+        )
+        article_heading = article_link.find("span").text.strip()
+        link = article_link.get("href")
+        articles[article_heading] = link
     return articles
 
 
@@ -73,22 +98,20 @@ def scrape():
     # refresh_token = auth_user['refresh_token']
 
     try:
-        for heading, link in scrape_cnn().items():
+        for heading, link in (scrape_cnn() | scrape_boston_herald()).items():
             response = (
                 supabase.table("articles")
-                .insert({ "article_heading": heading, "article_url": link})
+                .insert({"article_heading": heading, "article_url": link})
                 .execute()
             )
         # print("Insertion successful:", response)
     except Exception as e:
         console.print(f"Error: \n {e}", style="danger")
     else:
-        console.print("The news was successfully retrieved and added to the database", style="success")
-
-    # print(response.text)
-    # print(url)
-    # print(key)
-    # cnn = scrape_cnn()
+        console.print(
+            "The news was successfully retrieved and added to the database",
+            style="success",
+        )
 
 
 if __name__ == "__main__":
